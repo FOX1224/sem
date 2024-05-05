72c03f015b726837b03389d013d453627ce64bfb
FOX1224 <103663464+FOX1224@users.noreply.github.com>
2024-05-05 23:57:58 +0800

    Update MovieModule.java

--- src/MovieModule.java
+++ src/MovieModule.java
@@ -1,10 +1,15 @@
-//handle the logic for displaying available movies and selecting one based on user input.
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MovieModule {
     private Movie[] movies;
 
     public MovieModule() {
         // Initialize the movie array here
+        initMovies();
+    }
+
+    private void initMovies() {
         this.movies = new Movie[] {
             new Movie("Star Wars", "English", "Sci-Fi", "3:00PM", "13+", 2),
             new Movie("Your Name", "Japanese", "Fiction", "5:00PM", "13+", 5),
@@ -12,6 +17,18 @@ public class MovieModule {
             new Movie("Among Us", "English", "Sci-Fi", "8:00PM", "13+", 3),
             new Movie("Imposter", "English", "History", "9:00PM", "18+", 4)
         };
+    }
+
+    // Method to add new movies
+    public void addMovie(Movie movie) {
+        // Create a new array with increased size to accommodate the new movie
+        Movie[] updatedMovies = new Movie[movies.length + 1];
+        // Copy existing movies to the new array
+        System.arraycopy(movies, 0, updatedMovies, 0, movies.length);
+        // Add the new movie to the end of the array
+        updatedMovies[movies.length] = movie;
+        // Update the movies array reference to point to the new array
+        movies = updatedMovies;
     }
 
     public void displayMovies() {
